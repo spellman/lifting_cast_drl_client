0e5ef269775e63121d375b14de1dc4c2ede11e87
Cort Spellman <spellman.cort@gmail.com>
2018-04-19 04:59:26 -0400

    liftingcast_to_drl: Rename g_db_admin_party -> g_db.

--- liftingcast_to_drl.py
+++ liftingcast_to_drl.py
@@ -136,7 +136,7 @@ Output file: {output_file}""".format(day_number=DAY_NUMBER,
 
 
 # Set up global database util for fetch and put
-g_db_admin_party = db_admin_party()
+g_db = db_admin_party()
 
 
 # Set up replication from liftingcast.com CouchDB to local CouchDB.
@@ -525,12 +525,11 @@ def advance_liftingcast_to_next_lifter(next_lifter_attempt):
     else:
         next_attempt_id = doc_id(next_lifter_attempt.attempt)
 
-    platform = g_db_admin_party.fetch_doc_from_db(local_db.database_url,
-                                                  PLATFORM_ID)
+    platform = g_db.fetch_doc_from_db(local_db.database_url, PLATFORM_ID)
     platform["currentAttemptId"] = next_attempt_id
     platform["clockState"] = "initial"
     platform["clockTimerLength"] = INITIAL_CLOCK_VALUE_IN_MILLISECONDS
-    g_db_admin_party.put_doc_to_db(local_db.database_url, platform)
+    g_db.put_doc_to_db(local_db.database_url, platform)
 
 
 
