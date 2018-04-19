d3d728c08b3c923575eb2bd0f7469aa45a192b17
Cort Spellman <spellman.cort@gmail.com>
2018-04-19 05:44:41 -0400

    Fix get_next_lifter check for whether lifter exists.

--- liftingcast_to_drl.py
+++ liftingcast_to_drl.py
@@ -368,7 +368,7 @@ def get_current_lifter():
 
 def get_next_lifter():
     lifting_order_to_be_done = get_lifting_order_to_be_done()
-    if len(lifting_order_to_be_done) > 1:
+    if len(lifting_order_to_be_done) > 0:
         return lifting_order_to_be_done[0]
     else:
         return None
