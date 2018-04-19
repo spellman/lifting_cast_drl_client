577480a65938e1f262bc1297e8f9c918b369ca27
Cort Spellman <spellman.cort@gmail.com>
2018-04-19 04:33:19 -0400

    Retry database puts that result in conflicts.
    We're taking the same approach as liftingcast.com in
    pouchActions.updateAttributeOnDocument

--- db_util.py
+++ db_util.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import urllib2
 
 
 
@@ -12,15 +13,39 @@ class db():
         return requests.get("{}/{}".format(db_url, doc_id),
                             auth=(self.meetid, self.passwd)).json()
 
+    def private_put_doc_to_db(self, db_url, d, num_tries, max_tries):
+        if num_tries > max_tries:
+            raise Exception("Error putting document for update.")
+        else:
+            try:
+                return requests.put("{}/{}".format(db_url, d["_id"]),
+                                    auth=(self.meetid, self.passwd),
+                                    data = json.dumps(d))
+            except urllib2.HTTPError as e:
+                if e.code == 409:
+                    self.private_put_doc_to_db(db_url, d, num_tries + 1, max_tries)
+                else:
+                    raise e
+
     def put_doc_to_db(self, db_url, d):
-        return requests.put("{}/{}".format(db_url, d["_id"]),
-                            auth=(self.meetid, self.passwd),
-                            data = json.dumps(d))
+        return self.private_put_doc_to_db(db_url, d, 0, 10)
 
 class db_admin_party():
     def fetch_doc_from_db(self, db_url, doc_id):
         return requests.get("{}/{}".format(db_url, doc_id)).json()
 
+    def private_put_doc_to_db(self, db_url, d, num_tries, max_tries):
+        if num_tries > max_tries:
+            raise Exception("Error putting document for update.")
+        else:
+            try:
+                return requests.put("{}/{}".format(db_url, d["_id"]),
+                                    data = json.dumps(d))
+            except urllib2.HTTPError as e:
+                if e.code == 409:
+                    self.private_put_doc_to_db(db_url, d, num_tries + 1, max_tries)
+                else:
+                    raise e
+
     def put_doc_to_db(self, db_url, d):
-        return requests.put("{}/{}".format(db_url, d["_id"]),
-                            data = json.dumps(d))
+        return self.private_put_doc_to_db(db_url, d, 0, 10)
